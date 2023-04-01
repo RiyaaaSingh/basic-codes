e4e0e054fc54c16093214872f521fed8a9fe8626
RiyaaaSingh <114330695+RiyaaaSingh@users.noreply.github.com>
2023-04-01 23:38:28 +0530

    Create Add two integers.c

---
+++ Add two integers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {    
+
+    int number1, number2, sum;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+
+    // calculate the sum
+    sum = number1 + number2;      
+    
+    printf("%d + %d = %d", number1, number2, sum);
+    return 0;
+}
+
+
+
+
+output
+Enter two integers: 10
+11
+10 + 11 = 21
