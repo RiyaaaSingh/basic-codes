f9b99c5a45988ef7e31bbdcae80efeeaf349d1a5
RiyaaaSingh <114330695+RiyaaaSingh@users.noreply.github.com>
2023-04-11 23:43:35 +0530

    Create Print an Integer (Entered by the User).c

---
+++ Print an Integer (Entered by the User).c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {   
+    int number;
+   
+    printf("Enter an integer: ");  
+    
+    // reads and stores input
+    scanf("%d", &number);
+
+    // displays output
+    printf("You entered: %d", number);
+    
+    return 0;
+}
+
+
+
+
+output
+Enter an integer: 25
+You entered: 25
