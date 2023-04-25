d47ecd0820f9a5b2fea4d885ad27da387065b39d
RiyaaaSingh <114330695+RiyaaaSingh@users.noreply.github.com>
2023-04-25 23:20:13 +0530

    Create Display Characters from A to Z Using Loop.c

---
+++ Display Characters from A to Z Using Loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    char c;
+    for (c = 'A'; c <= 'Z'; ++c)
+        printf("%c ", c);
+    return 0;
+}
+
+
+
+
+OUTPUT
+A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
